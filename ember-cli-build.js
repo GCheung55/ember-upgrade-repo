8f363bfaa8006e53271a913addbb65ba406fc889
Garrick <gcheung@fitbit.com>
2017-12-21 16:56:32 -0800

    Change JS directory.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    outputPaths: {
+      app: {
+         js: '/assets/scripts/my-app.js'
+      },
+      vendor: {
+        js: '/assets/scripts/vendor.js'
+      }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
